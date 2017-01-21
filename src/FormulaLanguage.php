0fa0bb8136a9a6f2188a04e6b7e4862e707d30c6
Ahmet Ozisik <ozisikahmet@gmail.com>
2017-01-21 14:18:34 +0300

    filters and result class are now deprecated

--- src/FormulaLanguage.php
+++ src/FormulaLanguage.php
@@ -2,23 +2,57 @@
 
 namespace Swiftmade\FEL;
 
+use Swiftmade\FEL\Casts\Collection;
+use Swiftmade\FEL\Casts\Stringy;
+use Swiftmade\FEL\Contracts\CastContract;
+
 class FormulaLanguage
 {
 
     protected $lexer;
     protected $parser;
+    protected $typeCasts;
 
     public function __construct()
     {
+        $this->typeCasts = [];
         $this->lexer = new Lexer();
         $this->parser = new Parser();
+
+        $this->registerTypeCast(new Stringy);
+        $this->registerTypeCast(new Collection);
+    }
+
+    public function registerTypeCast(CastContract $cast)
+    {
+        $this->typeCasts[$cast->type()] = $cast;
     }
 
     public function evaluate($code, array $variables = [])
     {
-        return $this->parser->parse(
+        $result = $this->parser->parse(
             $this->lexer->tokenize($code),
             $variables
         );
+
+        return $this->returnResult($result);
+    }
+
+    protected function resultType($result)
+    {
+        $type = gettype($result);
+        if ($type === 'object') {
+            return get_class($result);
+        }
+        return $type;
+    }
+
+    protected function returnResult($result)
+    {
+        $resultType = $this->resultType($result);
+        if (isset($this->typeCasts[$resultType])) {
+            return $this->typeCasts[$resultType]->cast($result);
+        }
+        return $result;
     }
 }
